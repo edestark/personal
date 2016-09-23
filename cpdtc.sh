8f24ff3f2139c7a64cdc873c4e58b7f760221f81
Edegard Santos <edesantos_@hotmail.com>
2016-09-23 10:33:42 -0300

    Create cpdtc.sh

---
+++ cpdtc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Created By:
+# Edegard Santos & Rafael Fiorentini
+#
+# This script will run over access logs to detect actions that have been done
+# through cPanel.
+# If you have any sugestions, please send an e-mail to edesantos_@hotmail.com
+#
+# Version 0.1 (Beta)
