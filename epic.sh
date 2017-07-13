366e33bb17dfe6ccfe262876603c67945354c805
Edegard Santos <edesantos_@hotmail.com>
2017-07-13 15:12:07 -0300

    Update epic.sh

--- epic.sh
+++ epic.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 #
-# trap ctrl-c
-trap ctrl_c INT
-
-ctrl_c() {
-        echo "** Sentaaaa o dedo no ctrl+c"
-}
 for i in $(seq 0 12345552 9999999999999); do echo "Número de pessoas enganadas:" $i; done
+
